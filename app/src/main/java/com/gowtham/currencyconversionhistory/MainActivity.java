58789c01287c5d8b481f57a509f5f540f435cc8f
Gowtham Shanmugaraj Ganesan <gowthamraj07@gmail.com>
2017-11-15 09:01:32 +0100

    Initial Commit

---
+++ app/src/main/java/com/gowtham/currencyconversionhistory/MainActivity.java
@@ -0,0 +1,87 @@
+package com.gowtham.currencyconversionhistory;
+
+import android.graphics.Color;
+import android.graphics.DashPathEffect;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private LineChart mChart;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mChart = (LineChart) findViewById(R.id.chart1);
+        mChart.setDrawGridBackground(false);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initLineChart();
+    }
+
+    private void initLineChart() {
+        ArrayList<Entry> values = new ArrayList<Entry>();
+
+        for (int i = 0; i < 10; i++) {
+            float val = i;
+            values.add(new Entry(i, val, getResources().getDrawable(android.support.v7.appcompat.R.drawable.abc_action_bar_item_background_material)));
+        }
+
+        LineDataSet set1;
+
+        if (mChart.getData() != null &&
+                mChart.getData().getDataSetCount() > 0) {
+            set1 = (LineDataSet)mChart.getData().getDataSetByIndex(0);
+            set1.setValues(values);
+            mChart.getData().notifyDataChanged();
+            mChart.notifyDataSetChanged();
+        } else {
+            // create a dataset and give it a type
+            set1 = new LineDataSet(values, "DataSet 1");
+
+            set1.setDrawIcons(false);
+
+            // set the line to be drawn like this "- - - - - -"
+            set1.enableDashedLine(10f, 5f, 0f);
+            set1.enableDashedHighlightLine(10f, 5f, 0f);
+            set1.setColor(Color.BLACK);
+            set1.setCircleColor(Color.BLACK);
+            set1.setLineWidth(1f);
+            set1.setCircleRadius(3f);
+            set1.setDrawCircleHole(false);
+            set1.setValueTextSize(9f);
+            set1.setDrawFilled(false);
+            set1.setFormLineWidth(1f);
+            set1.setFormLineDashEffect(new DashPathEffect(new float[]{10f, 5f}, 0f));
+            set1.setFormSize(15.f);
+
+
+            set1.setFillColor(Color.BLACK);
+
+
+            ArrayList<ILineDataSet> dataSets = new ArrayList<ILineDataSet>();
+            dataSets.add(set1); // add the datasets
+
+            // create a data object with the datasets
+            LineData data = new LineData(dataSets);
+
+            // set data
+            mChart.setData(data);
+        }
+    }
+}
